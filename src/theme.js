2c91c1bfcecb23f366801bf834370422d315ff7f
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-07-14 09:02:50 +0200

    social media site

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "1760a5",
+            light: "skyblue"
+        },
+        secondary: {
+            main: "888",
+        },
+        otherColor: {
+            main: "fff"
+        }
+    }
+})
